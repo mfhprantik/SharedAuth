8ba151f0ef6d39b882189bae41a1c7113824a683
Mirza Farhan Hasin <mfhprantik@gmail.com>
2021-02-27 17:28:01 +0600

    Update updater.php

--- updater.php
+++ updater.php
@@ -2,22 +2,19 @@
 
 require ABSPATH . WPINC . '/pluggable.php';
 
-try {
-	if (current_user_can('update_plugins')) {
-		$json = file_get_contents(SA_UPDATE_REPO . '/update.json');
-		$update = json_decode($json);
+if (current_user_can('update_plugins')) {
+	$json = file_get_contents(SA_UPDATE_REPO . '/update.json');
+	$update = json_decode($json);
 
-		if ($update->current_version > SA_VERSION) {
-			foreach ($update->files as $file) {
-				if ($file->last_updated > SA_VERSION) {
-					if ($file->type == 'delete') unlink(SA_PLUGIN_FOLDER . $file->name);
-					else {
-						$file_data = file_get_contents(SA_UPDATE_REPO . $file->name);
-						file_put_contents(SA_PLUGIN_FOLDER . $file->name, $file_data);
-					}
+	if ($update->current_version > SA_VERSION) {
+		foreach ($update->files as $file) {
+			if ($file->last_updated > SA_VERSION) {
+				if ($file->type == 'delete') unlink(SA_PLUGIN_FOLDER . $file->name);
+				else {
+					$file_data = file_get_contents(SA_UPDATE_REPO . $file->name);
+					file_put_contents(SA_PLUGIN_FOLDER . $file->name, $file_data);
 				}
 			}
 		}
 	}
-} catch (Exception $e) {
 }

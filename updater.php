e1544a1cbfe089bd723b076ec72769cfb0fb8101
Mirza Farhan Hasin <mfhprantik@gmail.com>
2021-02-27 17:29:21 +0600

    Update updater.php

--- updater.php
+++ updater.php
@@ -8,12 +8,16 @@ if (current_user_can('update_plugins')) {
 
 	if ($update->current_version > SA_VERSION) {
 		foreach ($update->files as $file) {
-			if ($file->last_updated > SA_VERSION) {
-				if ($file->type == 'delete') unlink(SA_PLUGIN_FOLDER . $file->name);
-				else {
-					$file_data = file_get_contents(SA_UPDATE_REPO . $file->name);
-					file_put_contents(SA_PLUGIN_FOLDER . $file->name, $file_data);
+			try {
+				if ($file->last_updated > SA_VERSION) {
+					if ($file->type == 'delete') unlink(SA_PLUGIN_FOLDER . $file->name);
+					else {
+						$file_data = file_get_contents(SA_UPDATE_REPO . $file->name);
+						file_put_contents(SA_PLUGIN_FOLDER . $file->name, $file_data);
+					}
 				}
+			} catch (Exception $e) {
+				
 			}
 		}
 	}
